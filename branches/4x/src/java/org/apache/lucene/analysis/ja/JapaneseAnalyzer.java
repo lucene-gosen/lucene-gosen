4b246d87da50aaaec9849d8dd355981a50706766
rcmuir <rcmuir@a288bef2-4cb4-bf82-9470-5f4567e72edf>
2011-04-13 20:23:41 +0000

    merge r26

--- branches/4x/src/java/org/apache/lucene/analysis/ja/JapaneseAnalyzer.java
+++ branches/4x/src/java/org/apache/lucene/analysis/ja/JapaneseAnalyzer.java
@@ -16,34 +16,108 @@ package org.apache.lucene.analysis.ja;
  * limitations under the License.
  */
 
+import java.io.IOException;
 import java.io.Reader;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.lucene.analysis.core.LowerCaseFilter;
 import org.apache.lucene.analysis.core.StopFilter;
+import org.apache.lucene.analysis.miscellaneous.KeywordMarkerFilter;
 import org.apache.lucene.analysis.util.StopwordAnalyzerBase;
+import org.apache.lucene.analysis.util.CharArraySet;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
+
 import org.apache.lucene.util.Version;
 
+/**
+ * Analyzer for Japanese which uses "Sen" morphological analyzer.
+ */
 public class JapaneseAnalyzer extends StopwordAnalyzerBase {
-  private final Set<String> stopTags;
+  private final Set<String> stoptags;
+  private final Set<?> stemExclusionSet;
 
-  public JapaneseAnalyzer(Version version, Set<?> stopwords, Set<String> stopTags) {
+  public static Set<?> getDefaultStopSet(){
+    return DefaultSetHolder.DEFAULT_STOP_SET;
+  }
+  
+  public static Set<String> getDefaultStopTags(){
+    return DefaultSetHolder.DEFAULT_STOP_TAGS;
+  }
+  
+  /**
+   * Atomically loads DEFAULT_STOP_SET, DEFAULT_STOP_TAGS in a lazy fashion once the 
+   * outer class accesses the static final set the first time.
+   */
+  private static class DefaultSetHolder {
+    static final Set<?> DEFAULT_STOP_SET;
+    static final Set<String> DEFAULT_STOP_TAGS;
+
+    static {
+      try {
+        DEFAULT_STOP_SET = loadStopwordSet(false, JapaneseAnalyzer.class, "stopwords_ja.txt", "#");
+        final CharArraySet tagset = loadStopwordSet(false, JapaneseAnalyzer.class, "stoptags_ja.txt", "#");
+        DEFAULT_STOP_TAGS = new HashSet<String>();
+        for (Object element : tagset) {
+          char chars[] = (char[]) element;
+          DEFAULT_STOP_TAGS.add(new String(chars));
+        }
+      } catch (IOException ex) {
+        // default set should always be present as it is part of the
+        // distribution (JAR)
+        throw new RuntimeException("Unable to load default stopword set");
+      }
+    }
+  }
+  
+  /**
+   * Create a JapaneseAnalyzer with the default stopwords and stoptags and no stemExclusionSet
+   */
+  public JapaneseAnalyzer(Version version) {
+    this(version, DefaultSetHolder.DEFAULT_STOP_SET, DefaultSetHolder.DEFAULT_STOP_TAGS, CharArraySet.EMPTY_SET);
+  }
+  
+  /**
+   * Create a JapaneseAnalyzer with the specified stopwords, stoptags, and stemExclusionSet
+   * 
+   * @param version lucene compatibility version
+   * @param stopwords a stopword set: words matching these (Surf
+   * @param stoptags a stoptags set: words containing these parts of speech will be removed from the stream.
+   * @param stemExclusionSet a stemming exclusion set: these words are ignored by 
+   *        {@link JapaneseBasicFormFilter} and {@link JapaneseKatakanaStemFilter}
+   */
+  public JapaneseAnalyzer(Version version, Set<?> stopwords, Set<String> stoptags, Set<?> stemExclusionSet) {
     super(version, stopwords);
-    this.stopTags = stopTags;
+    this.stoptags = stoptags;
+    this.stemExclusionSet = stemExclusionSet;
   }
 
+  /**
+   * Creates
+   * {@link org.apache.lucene.analysis.util.ReusableAnalyzerBase.TokenStreamComponents}
+   * used to tokenize all the text in the provided {@link Reader}.
+   * 
+   * @return {@link org.apache.lucene.analysis.util.ReusableAnalyzerBase.TokenStreamComponents}
+   *         built from a {@link JapaneseTokenizer} filtered with
+   *         {@link JapaneseWidthFilter}, {@link JapanesePunctuationFilter},
+   *         {@link JapanesePartOfSpeechStopFilter}, {@link JapaneseStopFilter},
+   *         {@link KeywordMarkerFilter} if a stem exclusion set is provided, 
+   *         {@link JapaneseBasicFormFilter}, {@link JapaneseKatakanaStemFilter},
+   *         and  {@link LowerCaseFilter}
+   */
   @Override
   protected TokenStreamComponents createComponents(String field, Reader reader) {
     Tokenizer tokenizer = new JapaneseTokenizer(reader);
     TokenStream stream = new JapaneseWidthFilter(tokenizer);
     stream = new JapanesePunctuationFilter(true, stream);
-    stream = new JapaneseBasicFormFilter(tokenizer);
-    stream = new JapanesePartOfSpeechStopFilter(true, tokenizer, stopTags);
+    stream = new JapanesePartOfSpeechStopFilter(true, stream, stoptags);
     stream = new StopFilter(matchVersion, stream, stopwords);
-    stream = new LowerCaseFilter(matchVersion, stream);
+    if (!stemExclusionSet.isEmpty())
+      stream = new KeywordMarkerFilter(stream, stemExclusionSet);
+    stream = new JapaneseBasicFormFilter(stream);
     stream = new JapaneseKatakanaStemFilter(stream);
+    stream = new LowerCaseFilter(matchVersion, stream);
     return new TokenStreamComponents(tokenizer, stream);
   }
 }
