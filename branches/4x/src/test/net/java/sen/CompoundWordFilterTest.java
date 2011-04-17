bca92257999f2601ff4f00bed68244b4367d651b
rcmuir <rcmuir@a288bef2-4cb4-bf82-9470-5f4567e72edf>
2011-04-17 19:22:39 +0000

    merge r64

--- branches/4x/src/test/net/java/sen/CompoundWordFilterTest.java
+++ branches/4x/src/test/net/java/sen/CompoundWordFilterTest.java
@@ -49,7 +49,7 @@ public class CompoundWordFilterTest {
 	@Test
 	public void testCompound1() throws IOException {
 
-		String testCompound = "駆け出し,3649,名詞,一般,*,*,*,*,駆け出し,カケダシ,カケダシ,\"駆け,3649,名詞,一般,*,*,*,*,駆け,カケ,カケ, 出し,3649,名詞,一般,*,*,*,*,出し,ダシ,ダシ,\"";
+		String testCompound = "駆け出し,3649,名詞,一般,*,*,*,*,*,カケダシ,カケダシ,\"駆け,3649,名詞,一般,*,*,*,*,*,カケ,カケ, 出し,3649,名詞,一般,*,*,*,*,*,ダシ,ダシ,\"";
 		File tempCompoundFile = File.createTempFile("tmp", "tmp");
 		tempCompoundFile.deleteOnExit();
 
@@ -59,8 +59,8 @@ public class CompoundWordFilterTest {
 		String testString = "駆け出し";
 
 		Token[] testTokens = new Token[] {
-				new Token ("駆け", 4281, 0, 2, new Morpheme ("名詞-一般", "*", "*", "駆け", new String[]{"カケ"}, new String[]{"カケ,"}, "")),
-				new Token ("出し", 4281, 2, 2, new Morpheme ("名詞-一般", "*", "*", "出し", new String[]{"ダシ"}, new String[]{"ダシ,"}, ""))
+				new Token ("駆け", 4281, 0, 2, new Morpheme ("名詞-一般", "*", "*", "*", new String[]{"カケ"}, new String[]{"カケ,"}, "")),
+				new Token ("出し", 4281, 2, 2, new Morpheme ("名詞-一般", "*", "*", "*", new String[]{"ダシ"}, new String[]{"ダシ,"}, ""))
 		};
 
 
@@ -83,7 +83,7 @@ public class CompoundWordFilterTest {
 	@Test
 	public void testCompound2() throws IOException {
 	
-		String testCompound = "駆け出し,3508,動詞,自立,*,*,五段・サ行,連用形,駆け出す,カケダシ,カケダシ,\"駆け,3649,名詞,一般,*,*,*,*,駆け,カケ,カケ, 出し,3649,名詞,一般,*,*,*,*,出し,ダシ,ダシ,\"";
+		String testCompound = "駆け出し,3508,動詞,自立,*,*,五段・サ行,連用形,駆け出す,カケダシ,カケダシ,\"駆け,3649,名詞,一般,*,*,*,*,*,カケ,カケ, 出し,3649,名詞,一般,*,*,*,*,*,ダシ,ダシ,\"";
 		File tempCompoundFile = File.createTempFile("tmp", "tmp");
 		tempCompoundFile.deleteOnExit();
 	
@@ -93,10 +93,10 @@ public class CompoundWordFilterTest {
 		String testString = "駆け出している";
 	
 		Token[] testTokens = new Token[] {
-				new Token ("駆け", 5205, 0, 2, new Morpheme ("名詞-一般", "*", "*", "駆け", new String[]{"カケ"}, new String[]{"カケ,"}, "")),
-				new Token ("出し", 5205, 2, 2, new Morpheme ("名詞-一般", "*", "*", "出し", new String[]{"ダシ"}, new String[]{"ダシ,"}, "")),
-				new Token ("て", 5551, 4, 1, new Morpheme ("助詞-接続助詞", "*", "*", "て", new String[]{"テ"}, new String[]{"テ"}, null)),
-				new Token ("いる", 5794, 5, 2, new Morpheme ("動詞-非自立", "一段", "基本形", "いる", new String[]{"イル"}, new String[]{"イル"}, null))
+				new Token ("駆け", 5205, 0, 2, new Morpheme ("名詞-一般", "*", "*", "*", new String[]{"カケ"}, new String[]{"カケ,"}, "")),
+				new Token ("出し", 5205, 2, 2, new Morpheme ("名詞-一般", "*", "*", "*", new String[]{"ダシ"}, new String[]{"ダシ,"}, "")),
+				new Token ("て", 5551, 4, 1, new Morpheme ("助詞-接続助詞", "*", "*", "*", new String[]{"テ"}, new String[]{"テ"}, null)),
+				new Token ("いる", 5794, 5, 2, new Morpheme ("動詞-非自立", "一段", "基本形", "*", new String[]{"イル"}, new String[]{"イル"}, null))
 		};
 	
 	
