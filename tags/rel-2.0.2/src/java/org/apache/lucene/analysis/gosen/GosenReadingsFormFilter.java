eaef071823fa242d6535ec8df4b1b271b9f85b87
johtani@gmail.com <johtani@gmail.com@a288bef2-4cb4-bf82-9470-5f4567e72edf>
2012-06-07 08:44:16 +0000

    release 2.0.2

---
+++ tags/rel-2.0.2/src/java/org/apache/lucene/analysis/gosen/GosenReadingsFormFilter.java
@@ -0,0 +1,71 @@
+package org.apache.lucene.analysis.gosen;
+
+/**
+ * Copyright 2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.lucene.analysis.KeywordMarkerFilter;
+import org.apache.lucene.analysis.TokenFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.gosen.tokenAttributes.ReadingsAttribute;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.KeywordAttribute;
+
+/**
+ * Replaces term text with the {@link ReadingsAttribute}.
+ * <p>
+ * To prevent terms from being replaced use an instance of
+ * {@link KeywordMarkerFilter} or a custom {@link TokenFilter} that sets
+ * the {@link KeywordAttribute} before this {@link TokenStream}.
+ * </p>
+ */
+public final class GosenReadingsFormFilter extends TokenFilter {
+  
+  private boolean romanized;
+  private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
+  private final ReadingsAttribute readingsAtt = addAttribute(ReadingsAttribute.class);
+  private final KeywordAttribute keywordAtt = addAttribute(KeywordAttribute.class);
+
+  public GosenReadingsFormFilter(TokenStream input) {
+    this(input, false);
+  }
+
+  public GosenReadingsFormFilter(TokenStream input, boolean romanized) {
+    super(input);
+    this.romanized = romanized;
+  }
+
+  @Override
+  public boolean incrementToken() throws IOException {
+    if (input.incrementToken()) {
+      if (!keywordAtt.isKeyword()) {
+        List<String> readings = readingsAtt.getReadings();
+        if (readings != null){ 
+          StringBuilder sb = new StringBuilder();
+          for(String reading : readings){
+            sb.append(romanized ? ToStringUtil.getRomanization(reading) : reading);
+          }
+          termAtt.setEmpty().append(sb.toString());
+        }
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
---
+++ tags/rel-2.0.2/src/test/org/apache/lucene/analysis/gosen/TestGosenAnalyzer.java
@@ -0,0 +1,59 @@
+package org.apache.lucene.analysis.gosen;
+
+/**
+ * Copyright 2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.IOException;
+
+import net.java.sen.SenTestUtil;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.BaseTokenStreamTestCase;
+import org.apache.lucene.analysis.gosen.GosenAnalyzer;
+
+/**
+ * Simple tests for {@link GosenAnalyzer} 
+ */
+public class TestGosenAnalyzer extends BaseTokenStreamTestCase {
+  /** This test fails with NPE when the 
+   * stopwords file is missing in classpath */
+  public void testResourcesAvailable() {
+    new GosenAnalyzer(TEST_VERSION_CURRENT, SenTestUtil.IPADIC_DIR);
+  }
+  
+  /**
+   * An example sentence, test removal of particles, etc by POS,
+   * lemmatization with the basic form, and that position increments
+   * and offsets are correct.
+   */
+  public void testBasics() throws IOException {
+    assertAnalyzesTo(new GosenAnalyzer(TEST_VERSION_CURRENT, SenTestUtil.IPADIC_DIR), "多くの学生が試験に落ちた。",
+      new String[] { "多く", "学生", "試験", "落ちる" },
+      new int[] { 0, 3, 6,  9 },
+      new int[] { 2, 5, 8, 11 },
+      new int[] { 1, 2, 2,  2 }
+    );
+  }
+  
+  /**
+   * Analyzes random unicode strings, to ensure no exception
+   * (results could be completely bogus, but makes sure we don't crash on some input)
+   */
+  public void testReliability() throws IOException {
+    Analyzer analyzer = new GosenAnalyzer(TEST_VERSION_CURRENT, SenTestUtil.IPADIC_DIR);
+    checkRandomData(random, analyzer, 10000);
+  }
+}
