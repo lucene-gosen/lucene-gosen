047507637371e726248104aceae22f2d86aada8e
johtani <johtani@a288bef2-4cb4-bf82-9470-5f4567e72edf>
2013-05-06 13:36:28 +0000

    for 4.3.0 release

---
+++ tags/rel-4.3.0/src/java/org/apache/solr/analysis/GosenPunctuationFilterFactory.java
@@ -0,0 +1,50 @@
+package org.apache.solr.analysis;
+
+/**
+ * Copyright 2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.Map;
+
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.gosen.GosenPunctuationFilter;
+import org.apache.lucene.analysis.util.TokenFilterFactory;
+
+/**
+ * Factory for {@link GosenPunctuationFilter}.
+ * <pre class="prettyprint" >
+ * &lt;fieldType name="text_ja" class="solr.TextField"&gt;
+ *   &lt;analyzer&gt;
+ *     &lt;tokenizer class="solr.GosenTokenizerFactory"/&gt;
+ *     &lt;filter class="solr.GosenPunctuationFilterFactory" 
+ *             enablePositionIncrements="true"/&gt;
+ *   &lt;/analyzer&gt;
+ * &lt;/fieldType&gt;</pre>
+ */
+public class GosenPunctuationFilterFactory extends TokenFilterFactory {
+  private final boolean enablePositionIncrements;
+
+  public GosenPunctuationFilterFactory(Map<String,String> args) {
+    super(args);
+    enablePositionIncrements = getBoolean(args, "enablePositionIncrements", false);
+    if (!args.isEmpty()) {
+      throw new IllegalArgumentException("Unknown parameters: " + args);
+    }
+  }
+
+  public TokenStream create(TokenStream stream) {
+    return new GosenPunctuationFilter(enablePositionIncrements, stream);
+  }
+}
