733ca96a4b3a5b8ac524512a655ca26d77b7f56b
johtani <johtani@a288bef2-4cb4-bf82-9470-5f4567e72edf>
2013-10-24 17:20:19 +0000

    issue #45  change libraries ver 4.5.0

---
+++ tags/rel-4.5.1/src/java/org/apache/solr/analysis/GosenBasicFormFilterFactory.java
@@ -0,0 +1,47 @@
+package org.apache.solr.analysis;
+
+/**
+ * Copyright 2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.gosen.GosenBasicFormFilter;
+import org.apache.lucene.analysis.util.TokenFilterFactory;
+
+import java.util.Map;
+
+/** 
+ * Factory for {@link GosenBasicFormFilter}.
+ * <pre class="prettyprint" >
+ * &lt;fieldType name="text_ja" class="solr.TextField"&gt;
+ *   &lt;analyzer&gt;
+ *     &lt;tokenizer class="solr.GosenTokenizerFactory"/&gt;
+ *     &lt;filter class="solr.GosenBasicFormFilterFactory"/&gt;
+ *   &lt;/analyzer&gt;
+ * &lt;/fieldType&gt;</pre>
+ */
+public class GosenBasicFormFilterFactory extends TokenFilterFactory {
+
+  public GosenBasicFormFilterFactory(Map<String, String> args) {
+    super(args);
+    if (!args.isEmpty()) {
+      throw new IllegalArgumentException("Unknown parameters: " + args);
+    }
+  }
+
+  public TokenStream create(TokenStream stream) {
+    return new GosenBasicFormFilter(stream);
+  }
+}
