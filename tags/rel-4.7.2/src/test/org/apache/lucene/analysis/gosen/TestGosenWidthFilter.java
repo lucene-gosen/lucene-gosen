734468936eeac24821b88407edfcb55e81ef088f
johtani <johtani@a288bef2-4cb4-bf82-9470-5f4567e72edf>
2014-04-15 09:39:41 +0000

    for release 4.7.2

---
+++ tags/rel-4.7.2/src/test/org/apache/lucene/analysis/gosen/TestGosenWidthFilter.java
@@ -0,0 +1,65 @@
+package org.apache.lucene.analysis.gosen;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.BaseTokenStreamTestCase;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.core.WhitespaceTokenizer;
+
+/**
+ * Tests for {@link GosenWidthFilter}
+ */
+public class TestGosenWidthFilter extends BaseTokenStreamTestCase {
+  private Analyzer analyzer = new Analyzer() {
+    @Override
+    protected TokenStreamComponents createComponents(String fieldName, Reader reader) {
+      Tokenizer source = new WhitespaceTokenizer(TEST_VERSION_CURRENT, reader);
+      return new TokenStreamComponents(source, new GosenWidthFilter(source));
+    }
+  };
+  
+  /**
+   * Full-width ASCII forms normalized to half-width (basic latin)
+   */
+  public void testFullWidthASCII() throws IOException {
+    assertAnalyzesTo(analyzer, "Ｔｅｓｔ １２３４",
+      new String[] { "Test",  "1234" });
+  }
+  
+  /**
+   * Half-width katakana forms normalized to standard katakana.
+   * A bit trickier in some cases, since half-width forms are decomposed
+   * and voice marks need to be recombined with a preceding base form. 
+   */
+  public void testHalfWidthKana() throws IOException {
+    assertAnalyzesTo(analyzer, "ｶﾀｶﾅ",
+      new String[] { "カタカナ" });
+    assertAnalyzesTo(analyzer, "ｳﾞｨｯﾂ",
+      new String[] { "ヴィッツ" });
+    assertAnalyzesTo(analyzer, "ﾊﾟﾅｿﾆｯｸ",
+      new String[] { "パナソニック" });
+  }
+  
+  public void testRandomData() throws IOException {
+    checkRandomData(random(), analyzer, 10000);
+  }
+}
