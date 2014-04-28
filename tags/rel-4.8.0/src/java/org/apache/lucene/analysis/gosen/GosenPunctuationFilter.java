8e67b69c89be8ea9e9b1256f8a12052b015e09a5
johtani <johtani@a288bef2-4cb4-bf82-9470-5f4567e72edf>
2014-04-28 07:51:52 +0000

    for release 4.8.0

---
+++ tags/rel-4.8.0/src/java/org/apache/lucene/analysis/gosen/GosenPunctuationFilter.java
@@ -0,0 +1,70 @@
+package org.apache.lucene.analysis.gosen;
+
+/**
+ * Copyright 2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.IOException;
+
+import org.apache.lucene.analysis.util.FilteringTokenFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.util.Version;
+
+/**
+ * Removes punctuation tokens
+ */
+public final class GosenPunctuationFilter extends FilteringTokenFilter {
+  private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
+
+  /** @deprecated enablePositionIncrements=false is not supported anymore as of Lucene 4.4. */
+  @Deprecated
+  public GosenPunctuationFilter(Version version, boolean enablePositionIncrements, TokenStream input) {
+    super(version, enablePositionIncrements, input);
+  }
+
+  public GosenPunctuationFilter(Version version, TokenStream input) {
+    super(version, input);
+  }
+
+  @Override
+  protected boolean accept() throws IOException {
+    return termAtt.length() > 0 && !isPunctuation(termAtt.buffer()[0]);
+  }
+  
+  static final boolean isPunctuation(char ch) {
+    switch(Character.getType(ch)) {
+      case Character.SPACE_SEPARATOR:
+      case Character.LINE_SEPARATOR:
+      case Character.PARAGRAPH_SEPARATOR:
+      case Character.CONTROL:
+      case Character.FORMAT:
+      case Character.DASH_PUNCTUATION:
+      case Character.START_PUNCTUATION:
+      case Character.END_PUNCTUATION:
+      case Character.CONNECTOR_PUNCTUATION:
+      case Character.OTHER_PUNCTUATION:
+      case Character.MATH_SYMBOL:
+      case Character.CURRENCY_SYMBOL:
+      case Character.MODIFIER_SYMBOL:
+      case Character.OTHER_SYMBOL:
+      case Character.INITIAL_QUOTE_PUNCTUATION:
+      case Character.FINAL_QUOTE_PUNCTUATION:
+        return true;
+      default:
+        return false;
+    }
+  }
+}
